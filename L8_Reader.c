03e2f055f5be9a1b4eb0e4362ade60998f822d3c
GNithish10 <128487193+GNithish10@users.noreply.github.com>
2024-12-25 19:10:25 +0530

    Add files via upload

---
+++ L8_Reader.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <fcntl.h>
+#include <unistd.h>
+#define FIFO_NAME "myfifo"
+int main()
+ {
+    int fd;
+    char buffer[100];
+    // Open the FIFO for reading
+    fd = open(FIFO_NAME, O_RDONLY);
+    if (fd == -1) {
+        perror("open");
+        exit(EXIT_FAILURE);
+    }
+    // Read the message from the FIFO
+    ssize_t bytesRead = read(fd, buffer, sizeof(buffer));
+    if (bytesRead > 0) {
+        printf("Reader: Message received - %s", buffer);
+    }
+
+    // Close the FIFO
+    close(fd);
+    // Optionally remove the FIFO
+    unlink(FIFO_NAME);
+    return 0;
+}
